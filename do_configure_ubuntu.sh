475f1d4bc910b52e7b85e314680823a3a6bccd1f
Takuya Nishimoto <nishimoto@m.ieice.org>
2010-11-04 14:30:09 +0900

    configure for ubuntu

---
+++ do_configure_ubuntu.sh
@@ -0,0 +1,7 @@
+# do_configure_ubuntu.sh
+export CXX='g++'
+export CC='gcc'
+HTSENGINE=`pwd`/../htsengineapi
+./configure --with-hts-engine-header-path=$HTSENGINE/include \
+  --with-hts-engine-library-path=$HTSENGINE/lib
+
