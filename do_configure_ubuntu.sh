96d9e56577061c52648353421952a8094b015cc5
nishi <nishi@rcpt.cc>
2010-12-17 10:33:01 +0000

    charset

--- do_configure_ubuntu.sh
+++ do_configure_ubuntu.sh
@@ -3,5 +3,6 @@ export CXX='g++'
 export CC='gcc'
 HTSENGINE=`pwd`/../htsengineapi
 ./configure --with-hts-engine-header-path=$HTSENGINE/include \
-  --with-hts-engine-library-path=$HTSENGINE/lib
+  --with-hts-engine-library-path=$HTSENGINE/lib \
+  --with-charset=shift_jis
 
