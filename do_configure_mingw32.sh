47e0eb319dca3e8f396421ceff055b51be0ecaa6
Takuya Nishimoto <nishimotz@gmail.com>
2011-02-14 23:49:56 +0900

    document update

--- do_configure_mingw32.sh
+++ do_configure_mingw32.sh
@@ -4,9 +4,12 @@
 # autoconf
 # make
 # cd lib
-# make
+# make -f Makefile.mingw32 clean
+# make -f Makefile.mingw32
+# strip libopenjtalk.dll
 export CXX='g++ -mno-cygwin'
 export CC='gcc -mno-cygwin'
-./configure --with-hts-engine-header-path=/cygdrive/c/work/github/htsengineapi/include \
-  --with-hts-engine-library-path=/cygdrive/c/work/github/htsengineapi/lib \
+HTSENGINE_DIR=/cygdrive/c/work/github/htsengineapi
+./configure --with-hts-engine-header-path=$HTSENGINE_DIR/include \
+  --with-hts-engine-library-path=$HTSENGINE_DIR/lib \
   --build=i686-pc-mingw32 --with-charset=shift_jis 
