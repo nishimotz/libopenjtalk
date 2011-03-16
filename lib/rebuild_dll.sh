aabf22807895b3c45791fb18501fe3fce1d6a773
Takuya Nishimoto <nishimotz@gmail.com>
2011-03-16 23:29:23 +0900

    rebuild script update

--- lib/rebuild_dll.sh
+++ lib/rebuild_dll.sh
@@ -1,3 +1,22 @@
+# for cygwin gcc3
+export CXX='g++ -mno-cygwin'
+export CC='gcc -mno-cygwin'
+sh /usr/bin/set-gcc-default-3.sh
+echo 'building htsengineapi'
+pushd ../../htsengineapi
+  make clean
+  autoconf
+  ./configure --build=i686-pc-mingw32
+  make
+popd
+echo 'building libopenjtalk'
+pushd ..
+  make clean
+  sh do_configure_mingw32.sh
+  autoconf
+  make
+popd
+echo 'building DLL'
+make -f Makefile.mingw32 clean
 make -f Makefile.mingw32 clean-dll htsengineapi njd_set_unvoiced_vowel libjpcommon libopenjtalk.dll
 strip libopenjtalk.dll
-
--- mecab-naist-jdic/make_jdic.sh
+++ mecab-naist-jdic/make_jdic.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # for Ubuntu Linux 9.04
-
 # usage:
-# python eng_dic_maker.py
-# python tankan_dic_maker.py
 # sh make_jdic.sh 
 
 SRCDIR=.
-OUTDIR=/home/nishi/code/jtalk/dic
+OUTDIR=/work/jtalk-dic
 
 mkdir -p $OUTDIR
-/usr/lib/mecab/mecab-dict-index -d $SRCDIR -o $OUTDIR -f EUC-JP -c Shift_JIS
+python eng_dic_maker.py # uses bep-eng.dic
+python tankan_dic_maker.py # uses nvdajp_dic.py
+python custom_dic_maker.py
+#/usr/lib/mecab/mecab-dict-index -d $SRCDIR -o $OUTDIR -f EUC-JP -c Shift_JIS
+/usr/lib/mecab/mecab-dict-index -d $SRCDIR -o $OUTDIR -f cp932 -c cp932
 cp $SRCDIR/dicrc $OUTDIR
