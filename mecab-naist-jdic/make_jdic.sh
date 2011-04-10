a646b78e86b0ebf2f389ac1847c25c4307ff0069
Takuya Nishimoto <nishimotz@gmail.com>
2011-04-10 23:05:19 +0900

    roma_dic_maker, duplicate tankan items

--- mecab-naist-jdic/make_jdic.sh
+++ mecab-naist-jdic/make_jdic.sh
@@ -11,6 +11,7 @@ mkdir -p $OUTDIR
 python eng_dic_maker.py # uses bep-eng.dic
 python tankan_dic_maker.py # uses nvdajp_dic.py
 python custom_dic_maker.py
+python roma_dic_maker.py
 /usr/lib/mecab/mecab-dict-index -d $SRCDIR -o $OUTDIR -f cp932 -c cp932
 cp $SRCDIR/dicrc $OUTDIR
 cp -av $OUTDIR/* $OUTDIR2
--- mecab-naist-jdic/tankan_dic_maker.py
+++ mecab-naist-jdic/tankan_dic_maker.py
@@ -12,15 +12,25 @@ sys.path.append(IN_DIR)
 import nvdajp_dic
 import re
 
-if __name__ == '__main__':
+def contains_hankaku_katakana(k):
+	# hankaku katakana check
+	# http://programmer-toy-box.sblo.jp/article/24644519.html
+	regexp = re.compile(r'(?:\xEF\xBD[\xA1-\xBF]|\xEF\xBE[\x80-\x9F])|[\x20-\x7E]')
+	result = regexp.search(k.encode('utf-8'))
+	if result != None: return True
+	return False
+
+def version1():
 	with open(OUT_FILE, "w") as file:
 		for k,v in nvdajp_dic.dic1.iteritems():
 			# ＡＬＴ,1345,1345,6913,名詞,一般,*,*,*,*,ＡＬＴ,オルト,オルト,0/3,C1
 			try:
+				# hankaku katakana check
 				# http://programmer-toy-box.sblo.jp/article/24644519.html
-				regexp = re.compile(r'(?:\xEF\xBD[\xA1-\xBF]|\xEF\xBE[\x80-\x9F])|[\x20-\x7E]')
-				result = regexp.search(k.encode('utf-8'))
-				if result != None : continue
+				#regexp = re.compile(r'(?:\xEF\xBD[\xA1-\xBF]|\xEF\xBE[\x80-\x9F])|[\x20-\x7E]')
+				#result = regexp.search(k.encode('utf-8'))
+				#if result != None : continue
+				if contains_hankaku_katakana(k): continue
 				dummy = k.encode('cp932')
 				k1 = k
 				y = v[4]
@@ -30,3 +40,32 @@ if __name__ == '__main__':
 				file.write(s.encode('cp932'))
 			except Exception, e:
 				print e
+
+def version2():
+	import csv
+	jdic_tankan = {}
+	reader = csv.reader(open('sjis-naist-jdic.csv', 'r'))
+	for row in reader:
+		hyousou = row[0].decode('cp932')
+		if len(hyousou) == 1:
+			jdic_tankan[hyousou] = row
+	with open(OUT_FILE, "w") as file:
+		for k,v in nvdajp_dic.dic1.iteritems():
+			if contains_hankaku_katakana(k): continue
+			if k in jdic_tankan:
+				continue # print "%s in hyousou" % k.encode('cp932')
+			try:
+				dummy = k.encode('cp932')
+			except Exception, e:
+				print e
+				continue
+			k1 = k
+			y = v[4]
+			y = y.replace(' ', '')
+			mora_count = len(y)
+			# 表層形,左文脈ID,右文脈ID,コスト,品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+			s = u"%s,-1,-1,15000,記号,一般,*,*,*,*,%s,%s,%s,0/%d,C0\n" % (k1,k1,y,y,mora_count)
+			file.write(s.encode('cp932'))
+	
+if __name__ == '__main__':
+	version2()
