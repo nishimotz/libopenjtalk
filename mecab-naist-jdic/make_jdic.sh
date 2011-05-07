41e6bb604daf386544c8f67849919d3a052309f6
Takuya Nishimoto <nishimotz@gmail.com>
2011-05-07 23:17:52 +0900

    use _temp for build dic

--- mecab-naist-jdic/make_jdic.sh
+++ mecab-naist-jdic/make_jdic.sh
@@ -3,7 +3,6 @@
 # usage:
 # sh make_jdic.sh 
 
-SRCDIR=.
 OUTDIR=/work/nvda/miscdep/source/nvdajptext/dic/
 OUTDIR2=/work/nvda/jp2011.1/source/nvdajptext/dic/
 
@@ -12,7 +11,14 @@ python eng_dic_maker.py # uses bep-eng.dic
 python tankan_dic_maker.py # uses nvdajp_dic.py
 python custom_dic_maker.py
 python roma_dic_maker.py
-/usr/lib/mecab/mecab-dict-index -d $SRCDIR -o $OUTDIR -f cp932 -c cp932
-cp $SRCDIR/dicrc $OUTDIR
+mkdir -p _temp
+cp nvdajp-eng-dic.csv nvdajp-tankan-dic.csv nvdajp-custom-dic.csv nvdajp-roma-dic.csv sjis-naist-jdic.csv _temp
+cp char.def feature.def left-id.def matrix.def pos-id.def rewrite.def right-id.def _temp
+cp unk.def.cp932 _temp/unk.def
+cp dicrc _temp
+cd _temp
+/usr/lib/mecab/mecab-dict-index -d . -o $OUTDIR -f cp932 -c cp932
+cd ..
+cp dicrc $OUTDIR
 echo "nvdajp-jtalk-dic" `date -u +%Y%m%d-%H%M%S` > $OUTDIR/DIC_VERSION
 cp -av $OUTDIR/* $OUTDIR2
